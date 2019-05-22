4820fe97a79f780993f4ba57bc6071eaf500f3f4
Daniel Ramos <ramos.danielferreira@gmail.com>
2019-05-21 23:26:43 -0300

    Adicionado enumviewmodel

---
+++ Back/Dani-TCC.Core/ViewModels/EnumViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dani_TCC.Core.ViewModels
+{
+    public class EnumViewModel
+    {
+        public EnumViewModel(int id, string description)
+        {
+            Id = id;
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+        }
+
+        public int Id { get; set; }
+        public String Description { get; set; }
+    }
+}
