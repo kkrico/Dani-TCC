f1ed805d5e79e8e7c14a0cb3b7c0314f1092635e
Daniel Ramos <ramos.danielferreira@gmail.com>
2019-06-20 03:24:33 -0300

    Coloca rescripts

--- Front/.rescriptsrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = config => {
-    config.optimization.splitChunks.automaticNameDelimiter = '_';
-    if (config.optimization.runtimeChunk) {
-        config.optimization.runtimeChunk = {
-            name: entrypoint => `runtime_${entrypoint.name}`
-        };
-    }
-    return config;
-};
